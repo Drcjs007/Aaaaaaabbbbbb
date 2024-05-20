6dfd949913a7e8243c23c7e5f177c312e5a59613
Drcjs007 <yateeshako1@gmail.com>
2024-05-20 13:48:39 +0530

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from .routes import register_routes
 
-def create_server():
-    app = Flask(__name__)
-    register_routes(app)
-    return app
+app = Flask(__name__)
 
-app = create_server()  # This ensures that app is available as an attribute
+from . import routes
