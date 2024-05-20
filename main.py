c09e563054a1788e6780ae7f4e2342963df3e1a8
Drcjs007 <yateeshako1@gmail.com>
2024-05-20 13:14:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import signal
 import asyncio
 from dotenv import load_dotenv
 from bot import app as bot_app
-from server import create_server
+from server import app as flask_app
 
 # Load environment variables
 load_dotenv()
 
 PORT = int(os.getenv("PORT", 5000))  # Default to port 5000 if PORT is not set
 
-server = create_server()
-
 async def main():
     await bot_app.start()
-    server.run(host="0.0.0.0", port=PORT)
+    flask_app.run(host="0.0.0.0", port=PORT)
 
 def shutdown(signal, frame):
     print("Received shutdown signal")
