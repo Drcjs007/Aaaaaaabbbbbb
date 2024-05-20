ef7e7168f114c41ff37626840cfc34ece755e171
Drcjs007 <yateeshako1@gmail.com>
2024-05-20 13:48:14 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,15 +1,3 @@
-import os
-from pyrogram import Client
-from dotenv import load_dotenv
+from .bot import create_app
 
-# Load environment variables
-load_dotenv()
-
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-# Import commands to register them
-from . import commands
+app = create_app()
